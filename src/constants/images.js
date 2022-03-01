ad12d149a8b13ac1f6899ef459e38753e34cbb4f
Tae <tae-hyung.k@clubware.co.nz>
2022-03-02 10:59:40 +1300

    images are replaced to jxr format to load faster

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,12 +1,12 @@
-import welcome from "../assets/welcome.jpeg";
-import pricing from "../assets/pricing.jpeg";
-import contacts from "../assets/contacts.jpeg";
-import logo from "../assets/Beralogo.jpg";
-import kakaoTalk from "../assets/kakao-talk.png";
-import work1 from "../assets/work-1.jpeg";
-import work2 from "../assets/work-2.jpeg";
-import work3 from "../assets/work-3.jpeg";
-import work4 from "../assets/work-4.jpeg";
+import welcome from "../assets/welcome.jxr";
+import pricing from "../assets/pricing.jxr";
+import contacts from "../assets/contacts.jxr";
+import logo from "../assets/Beralogo.jxr";
+import kakaoTalk from "../assets/kakao-talk.jxr";
+import work1 from "../assets/work-1.jxr";
+import work2 from "../assets/work-2.jxr";
+import work3 from "../assets/work-3.jxr";
+import work4 from "../assets/work-4.jxr";
 
 export default {
   welcome,
